a364c8d57ec1c1432d99a31e1af162abd29a87c8
mertsariyar <mertsariyar2@gmail.com>
2025-12-11 14:35:12 +0300

    added constants.py file for constant variables, also import it to the main file.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
-
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     
 
 
