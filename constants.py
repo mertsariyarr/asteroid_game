d16b30b2382919b59396e771409b23849cae2b4b
mertsariyar <mertsariyar2@gmail.com>
2025-12-12 21:47:40 +0300

    added PLAYER_SPEED to constans for our player's speed

--- constants.py
+++ constants.py
@@ -3,3 +3,4 @@ SCREEN_HEIGHT = 720
 PLAYER_RADIUS = 20 # The Radius of the Player's Ship
 LINE_WIDTH = 2 # The width of the lines that draw the player's ship
 PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
