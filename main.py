a423c1cd3ebfb9b7db97d32cd245431b8c398c73
mertsariyar <mertsariyar2@gmail.com>
2025-12-12 21:12:34 +0300

    added player.py file for our player object with functionality (drawing player in the screen)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -8,6 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         log_state()
         
@@ -16,7 +18,9 @@ def main():
                 return
             
         screen.fill("black")
-        pygame.display.flip() 
+        player.draw(screen) 
+
+        pygame.display.flip()
         dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
 
        
