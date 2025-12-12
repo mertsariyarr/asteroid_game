721b7f5427d436e4fb7b49877cb146c850c2bcc8
mertsariyar <mertsariyar2@gmail.com>
2025-12-12 19:36:03 +0300

    import logger function to main and create infinite loop

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip() # Make Sure this method works at the end.
 
 
 if __name__ == "__main__":
