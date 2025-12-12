71244691fa4268c9ebbd4cd9f7118d706cd237f4
mertsariyar <mertsariyar2@gmail.com>
2025-12-12 21:22:35 +0300

    added PLAYER_TURN_SPEED = 300 variable to update our player's rotation and direction

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 PLAYER_RADIUS = 20 # The Radius of the Player's Ship
 LINE_WIDTH = 2 # The width of the lines that draw the player's ship
+PLAYER_TURN_SPEED = 300
