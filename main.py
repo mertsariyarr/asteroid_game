09ea2fc318f320b28b342ce47852a2b713a28039
mertsariyar <mertsariyar2@gmail.com>
2025-12-12 21:49:08 +0300

    added move method to our player, it updates the player's position each frame according to the keyboard print (w, s)

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
         screen.fill("black")
         player.draw(screen)
         player.update(dt) 
+        player.move(dt)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
