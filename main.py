f6b3f4eeae2e4274fdf501367817db2c150c9e0f
mertsariyar <mertsariyar2@gmail.com>
2025-12-12 19:53:47 +0300

    added clock object to main and delta time variable

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        pygame.display.flip() # Make Sure this method works at the end.
+        pygame.display.flip() 
+        dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
+
+       
 
 
 if __name__ == "__main__":
