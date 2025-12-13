4ea578eceb8a2e10f799b774c7ebb17163d6b4cf
mertsariyar <mertsariyar2@gmail.com>
2025-12-13 19:20:42 +0300

    i added new attribute (self.radius) i forget to add it when i create it

--- circleshape.py
+++ circleshape.py
@@ -11,6 +11,7 @@ class CircleShape(pygame.sprite.Sprite):
             super().__init__()
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
 
     def draw(self, screen):
         # must override
